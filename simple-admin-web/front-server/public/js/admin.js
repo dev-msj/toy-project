04fcf4e52acac2acd6996dba1b4c3a435c256eed
dev-msj <msj944@naver.com>
2022-06-29 16:59:07 +0900

    feat(simple-admin-web): 작업 내용 추가 및 수정용 modal 추가

--- simple-admin-web/front-server/public/js/admin.js
+++ simple-admin-web/front-server/public/js/admin.js
@@ -1,8 +1,17 @@
-const formModal = document.getElementById('contentFormModal');
-formModal?.addEventListener('show.bs.modal', event => {
-  const button = event.relatedTarget;
-  const content = button.getAttribute('data-bs-whatever');
-  const modalBodyInput = formModal.querySelector('.modal-body input');
+window.onload = () => {
+    const modal = document.getElementById("contentFormModal");
+    modal.addEventListener('show.bs.modal', (event) => {
+        const button = event.relatedTarget;
+        const content = button.getAttribute('data-bs-whatever');
 
-  modalBodyInput.value = content;
-});
+        const modalTitle = contentFormModal.querySelector('.modal-title')
+        const modalBodyInput = contentFormModal.querySelector('.modal-body input');
+
+        if (content === "") {
+            modalTitle.textContent = "New " + modalTitle.textContent;
+        } else {
+            modalTitle.textContent = "Update " + modalTitle.textContent;
+        }
+        modalBodyInput.value = content;
+    });
+}
