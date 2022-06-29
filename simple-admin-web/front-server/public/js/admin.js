f599f42d2e28e3021e41af05b91c82998e1b635d
dev-msj <msj944@naver.com>
2022-06-29 16:30:10 +0900

    feat(simple-admin-web): bootstrap, jquery를 npm 모듈에서 사용하도록 변경

--- simple-admin-web/front-server/public/js/admin.js
+++ simple-admin-web/front-server/public/js/admin.js
@@ -1,3 +1,8 @@
-// const formModal = document.getElementById('formModal');
-// formModal.addEventListener('show.bs.modal', event => {
-// });
+const formModal = document.getElementById('contentFormModal');
+formModal?.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget;
+  const content = button.getAttribute('data-bs-whatever');
+  const modalBodyInput = formModal.querySelector('.modal-body input');
+
+  modalBodyInput.value = content;
+});
